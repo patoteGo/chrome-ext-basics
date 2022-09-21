c7a4bb659c9990b3172c13af738c33b43bacbfe0
Patricio Gonzalez <patote.gonzalez@gmail.com>
2022-09-21 09:46:10 -0300

    storage API

--- start/popup.js
+++ start/popup.js
@@ -1,4 +1,5 @@
 const timeElement = document.getElementById('time')
+const nameElement = document.getElementById('name')
 const currentTime = new Date().toLocaleTimeString()
 timeElement.textContent = `The time is: ${currentTime}`
 
@@ -6,4 +7,8 @@ chrome.action.setBadgeText({
 	text: "TIME",
 }, () => {
 	console.log("Finished setting badge text.")
+})
+
+chrome.storage.sync.get(['name'], (res) => {
+	nameElement.textContent = `Your name is: ${res.name}`
 })
