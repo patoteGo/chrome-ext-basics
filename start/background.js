2b1776ea73bae4382b7e1742d49d45b5e7260e46
Patricio Gonzalez <patote.gonzalez@gmail.com>
2022-09-21 10:21:53 -0300

    alarm API

--- start/background.js
+++ start/background.js
@@ -1,9 +1,15 @@
-console.log('this is the background worker')
+chrome.alarms.create({
+	periodInMinutes: 1 / 60
+})
 
-console.log(this)
-let time = 0
-
-setInterval(() => {
-	time += 1
-	console.log(time);
-}, 1000);
+chrome.alarms.onAlarm.addListener((alarm) => {
+	chrome.storage.local.get(['timer'], (res) => {
+		const time = res.timer ?? 0
+		chrome.storage.local.set({
+			timer: time + 1,
+		})
+		chrome.action.setBadgeText({
+			text: `${time + 1}`
+		})
+	})
+})
